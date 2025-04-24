030aae656d916df26dd549e4cb953f3d2e44d834
Nilesh-Parmar-05 <nileshparmar01012024@gmail.com>
2025-04-24 18:30:21 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import initScrollReveal from "./scripts/scrollReveal";
-import initTiltEffect from "./scripts/tiltAnimation";
-import { targetElements, defaultProps } from "./data/scrollRevealConfig";
+import initScrollReveal from "./dist/scripts/scrollReveal";
+import initTiltEffect from "./dist/scripts/tiltAnimation";
+import { targetElements, defaultProps } from "./dist/data/scrollRevealConfig";
 
 initScrollReveal(targetElements, defaultProps);
 initTiltEffect();
